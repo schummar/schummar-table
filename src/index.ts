413256ff0184f9bada3988c988b153fe78da0e1f
Marco Schumacher <marco@schumacher.dev>
2021-06-29 09:57:42 +0200

    Added text match to export.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { DefaultFilter, DefaultFilterComponent } from './components/defaultFilterComponent';
 export { TextFilter, TextFilterComponent } from './components/textFilterComponent';
+export { termMatch, textMatch } from './misc/textMatch';
 export { ColumnContext, Table, TableContext, useColumnContext, useTableContext } from './table';
 export type { Column, Id, Rows, Sort, SortDirection, TableProps } from './types';
