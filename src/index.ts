20a172ad1dcab66d6e9cccab40d2a220fb20df1e
Marco Schumacher <marco@schumacher.dev>
2022-02-01 22:39:41 +0100

    fix: added missing types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { DefaultFilter, DefaultFilterComponent } from './components/defaultFilterComponent';
 export type { Filter } from './components/filterComponent';
 export { TextFilter, TextFilterComponent } from './components/textFilterComponent';
+export type { TableStateStorage } from './internalState/tableStateStorage';
 export { termMatch, textMatch } from './misc/textMatch';
-export { ColumnContext, Table, TableContext, useColumnContext, useTableContext } from './table';
+export { ColumnContext, configureTables, Table, TableContext, useColumnContext, useTableContext } from './table';
 export type { Column, Id, InternalColumn, Rows, Sort, SortDirection, TableProps } from './types';
