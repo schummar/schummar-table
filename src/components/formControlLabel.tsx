fa38fdcc4764a32a7ee5c88f49789b28efc84b55
Marco Schumacher <marco@schumacher.dev>
2022-02-16 00:32:18 +0100

    wip

--- src/components/formControlLabel.tsx
+++ src/components/formControlLabel.tsx
@@ -2,7 +2,18 @@ import { ReactNode } from 'react';
 
 export function FormControlLabel({ control, label }: { control: ReactNode; label: ReactNode }) {
   return (
-    <label css={{ display: 'flex', alignItems: 'center', cursor: 'pointer', '& > *': { cursor: 'pointer' } }}>
+    <label
+      css={{
+        display: 'flex',
+        alignItems: 'center',
+        cursor: 'pointer',
+        userSelect: 'none',
+
+        '& > *': {
+          cursor: 'pointer',
+        },
+      }}
+    >
       {control}
       <span css={{ marginLeft: 'var(--spacing)' }}>{label}</span>
     </label>
