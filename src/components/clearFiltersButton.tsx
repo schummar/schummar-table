9d97a0221005dd1c96cc469439eb95e99d052460
Michael F <rnichi@me.com>
2022-06-01 11:42:04 +0200

    feat: ClearFiltersButton improvements (#36)
    * ClearFiltersButton horizontally sticky
    
    * wip: horizontal scroll detected; width set correctly before scroll
    
    * wip: width is set correctly during expansion (issue when scrolling past)
    
    * wip: working properly for scrolling (issue: resize/other changes)
    
    * App test setup
    
    * simplify solution (removed manual calculations)
    
    * Revert "App test setup"
    
    This reverts commit b5517167cb143942c5310c51aefa7f9ee3bdd0ff.
    
    * Cleanup
    
    Co-authored-by: Michael Förg <michael.foerg@pentlandfirth.com>

---
+++ src/components/clearFiltersButton.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { defaultClasses } from '../theme/defaultTheme/defaultClasses';
+import { useTableContext, useTheme } from '..';
+
+export default function ClearFiltersButton<T>() {
+  const Button = useTheme((t) => t.components.Button);
+  const textClearFilters = useTheme((t) => t.text.clearFilters);
+  const table = useTableContext<T>();
+
+  return (
+    <div css={defaultClasses.clearFiltersButton}>
+      <Button
+        variant="text"
+        onClick={() => {
+          table.update((state) => {
+            state.activeColumns.forEach((column) => {
+              state.filterValues.delete(column.id);
+            });
+          });
+        }}
+      >
+        {textClearFilters}
+      </Button>
+    </div>
+  );
+}
